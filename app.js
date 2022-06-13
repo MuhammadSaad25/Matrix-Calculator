e53981afb5e4b0bfda7794eacb3c1ddeddc153d1
Muhammad Saad <99634425+MuhammadSaad25@users.noreply.github.com>
2022-06-13 21:39:25 +0500

    editing size

--- app.js
+++ app.js
@@ -40,9 +40,9 @@ function start() {
     for(let i = 0; i < first_input; i++) {
         for(let j = 0; j < second_input; j++){
 
-            matrix_input1.innerHTML += `<input type="number" class="matrix_input" id="matrix_input1_${i}_${j}" required value= "0" min = "0">`;
+            matrix_input1.innerHTML += `<input type="number" class="matrix_input" id="matrix_input1_${i}_${j}" required value= "0" min = "0" max = "999">`;
             
-            matrix_input2.innerHTML += `<input type="number" class="matrix_input" id="matrix_input2_${i}_${j}" required value= "0" min = "0">` ;
+            matrix_input2.innerHTML += `<input type="number" class="matrix_input" id="matrix_input2_${i}_${j}" required value= "0" min = "0" max = "999">` ;
         
             sum_input.innerHTML += `<input type="number" readonly class="matrix_input" id="sum_input_${i}_${j}" required placeholder ="0" >`;
         }
